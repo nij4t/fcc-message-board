b5e7bf6e7b8682ca34ffb85bc1cf5b13aafc058c
Displac3r <aspartame21@gmail.com>
2018-12-30 16:48:03 +0400

    Test suite for '/api/threads/:board'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,24 +16,72 @@ chai.use(chaiHttp);
 suite('Functional Tests', function() {
 
   suite('API ROUTING FOR /api/threads/:board', function() {
-    
-    suite('POST', function() {
-      
-    });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
 
+    var _id1;
+    
+    suite('POST a thread to a specific message board', function() {
+     
+      test('', done => {
+        chai.request(server)
+        .post('/api/threads/testboard')
+        .send({ text: 'test text', delete_password: 'testpasswd' })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          _id1 = res.body._id;
+          done();
+        })
+      })
+
+    });
+    
+    suite('GET an array of the most recent 10 bumped threads on the board', function() {
+      
+      test('', done => {
+        chai.request(server)
+        .get('/api/threads/testboard')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isArray(res.body);
+          assert.notProperty(res.body[0], 'delete_password');
+          assert.notProperty(res.body[0], 'reported');
+          done();
+        })
+      })
+      
+    });
+    
+    suite('PUT request to /api/threads/{board} => I can report a thread and change it\'s reported value to true', function() {
+      
+      test('', done => {
+        chai.request(server)
+        .put('/api/threads/testboard')
+        .send({ thread_id: _id1, reported: true })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.property(res.body, 'success');
+          done();
+        })
+      })
+
+    });
+
+    suite('DELETE request to /api/threads/{board} => I can delete a thread completely', function() {
+      
+      test('', done => {
+        chai.request(server)
+        .delete('/api/threads/testboard')
+        .send({ thread_id: _id1, delete_password: 'testpasswd' })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body);
+          assert.property(res.body, 'success');
+          done();
+        });
+      });
+
+    });
+    
   });
   
   suite('API ROUTING FOR /api/replies/:board', function() {
